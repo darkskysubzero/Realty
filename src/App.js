08e4e37a4a0611c4cb0d126a23c1b6540fffa0a5
Alin <alin@gmail.com>
2023-01-18 23:29:44 +0200

    installed react router and created navigation pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+// React Router Imports
+import { BrowserRouter as Router, } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
+// Pages Imports
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import ForgotPassword from "./Pages/ForgotPassword";
+import Offers from "./Pages/Offers";
+import PageNotFound from "./Pages/PageNotFound";
 
 function App() {
   return (
     <>
-      <h1 className="bg-red-300">Hello World</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
     </>
   );
 }
