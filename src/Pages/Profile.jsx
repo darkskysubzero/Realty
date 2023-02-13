7bd493f9368e23f38d91eeab60c81fbb68f6b943
Aysham Hameed <115636368+darkskysubzero@users.noreply.github.com>
2023-02-13 16:49:30 +0200

    added spinner

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
 import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
 
 export default function Profile() {
 
@@ -10,11 +13,12 @@ export default function Profile() {
 
 
     const [form, setForm] = useState({
-        name: "Default",
-        email: "default@gmail.com",
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email,
     })
-
     const { name, email } = form;
+
+    const [changeDetail, setChangeDetail] = useState(false);
 
 
     // User logging out
@@ -26,6 +30,39 @@ export default function Profile() {
 
     }
 
+    // Changing existing data method
+    const onChange = (e) => {
+        setForm((prev) => {
+            return {
+                ...prev,
+                [e.target.id]: e.target.value
+            }
+        })
+    }
+
+
+    // used to add change to database
+    const onSubmit = async (e) => {
+        try {
+            if (auth.currentUser.displayName !== name) {
+                //update display name in firebase auth
+                await updateProfile(auth.currentUser, {
+                    displayName: name
+                })
+
+                //update name in the firestore
+                const docRef = doc(db, "users", auth.currentUser.uid);
+                await updateDoc(docRef, {
+                    name: name
+                })
+
+                toast.success("Profile updated sucessfully", { toastId: "success" })
+            }
+        } catch (e) {
+            toast.error("Could not update the profile details")
+        }
+    }
+
     return (
         <>
             <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -33,16 +70,32 @@ export default function Profile() {
                 <div className='w-full md:w-[50%] mt-6 px-3'>
                     <form>
                         {/* Name Input  */}
-                        <input type="text" id='name' value={name} disabled
-                            className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6'
+                        <input type="text" id='name' value={name}
+                            disabled={!changeDetail}
+                            onChange={onChange}
+                            className={`w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6 ${changeDetail && "bg-red-200 focus:bg-red-200"}`}
                         />
                         {/* Email Input */}
-                        <input type="email" id='email' value={email} disabled
+                        <input type="email" id='email' value={email}
+
+                            disabled={!changeDetail}
+                            onChange={onChange}
                             className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6'
                         />
 
                         <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6'>
-                            <p>Do you want to change your name? <span className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>Edit</span></p>
+                            <p>Do you want to change your name? <span className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'
+                                onClick={() => {
+
+                                    //On click calling the submit method if state is set to save
+                                    changeDetail && onSubmit();
+                                    setChangeDetail((prev) => !prev);
+                                }
+                                }>
+                                {changeDetail ? "Save" : "Edit"}
+                            </span></p>
+
+
                             <p className='text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer' onClick={logout}>Sign out</p>
                         </div>
                     </form>
